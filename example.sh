b96a741ba3445dc5942f3405e3158278a554c068
Marcos Neves <marcos.neves@gmail.com>
2015-06-21 16:10:00 -0300

    bash example.sh to create a sample rails app with has_star

---
+++ example.sh
@@ -0,0 +1,58 @@
+# This script creates a new sample rails application, install/configure has_star
+
+rails new has_star_example --skip-test-unit --skip-bundle
+cd has_star_example
+echo "gem 'has_star', git: '/Users/neves/Dropbox/projects/rails/has_star'" >> Gemfile
+bundle install
+rails g resource post title --no-helper --no-assets
+
+# sample controller
+cat <<'EOF' >| app/controllers/posts_controller.rb
+class PostsController < ApplicationController
+  before_action -> { sleep 0.5 }, only: :toggle_star
+
+  def index
+    @posts = Post.all
+  end
+end
+EOF
+
+# basic routes
+cat <<'EOF' >| config/routes.rb
+Rails.application.routes.draw do
+  resources :posts, only: :index
+  root 'posts#index'
+end
+EOF
+
+rails g has_star post
+rake db:migrate
+
+# sample index
+cat <<'EOF' >| app/views/posts/index.html.erb
+<%= link_to 'all', posts_path %>
+ |
+<%= link_to 'starred', starred_posts_path %>
+ |
+<%= link_to 'unstarred', unstarred_posts_path %>
+
+<ul>
+  <% @posts.each do |post| %>
+  <li>
+    <%= has_star post %> |
+    <%= post.title %>
+  </li>
+  <% end %>
+</ul>
+EOF
+
+# populate database
+cat <<'EOF' >| db/seeds.rb
+10.times do |i|
+  Post.create! title: "Post #{i}", star: [true, false].sample
+end
+EOF
+
+rake db:seed
+# navigate to http://localhost:3000/
+rails server
